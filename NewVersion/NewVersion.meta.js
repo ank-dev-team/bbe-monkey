69db8c1cb04e41c70b76cddfd6336ff1e1646035
Seishin <ykohler@gmail.com>
2022-03-11 16:35:28 +0100

    Create NewVersion.meta.js

---
+++ NewVersion/NewVersion.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         NewVersion
+// @namespace    BBE CORP
+// @version      0.1
+// @description  Marque les nouvelles versions des PDF téléchargés
+// @author       Seishin
+// @match        http://www.black-book-editions.fr/mon_compte.php?a=pdf*
+// @match        https://www.black-book-editions.fr/mon_compte.php?a=pdf*
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require      https://code.jquery.com/jquery-3.5.1.slim.min.js
+// @run-at       document-end
+// @grant        none
+// @downloadURL https://github.com/ank-dev-team/bbe-monkey/raw/main/NewVersion/NewVersion.user.js
+// @updateURL   https://github.com/ank-dev-team/bbe-monkey/raw/main/NewVersion/NewVersion.meta.js
+// ==/UserScript==
